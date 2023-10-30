15f0c20517c50f589936218b188898b5f5a1046b
Or1gani <origani-chi@mail.ru>
2023-10-30 06:47:36 +0300

    working_version
    this project finally works!

---
+++ venv/Clear.py
@@ -0,0 +1,391 @@
+import time, sys, os
+
+
+class Player():
+    player_name = ""
+
+    big_ship = 1
+    medium_ship = 1
+    small_ship = 1
+    alive_ships = big_ship + small_ship + medium_ship
+
+class Field():
+    field_name = f"{Player.player_name}_field"
+
+    def __init__(self):
+        self.field = [
+            ["#", "#", "#", "#", "#", "#"],
+            ["#", "#", "#", "#", "#", "#"],
+            ["#", "#", "#", "#", "#", "#"],
+            ["#", "#", "#", "#", "#", "#"],
+            ["#", "#", "#", "#", "#", "#"],
+            ["#", "#", "#", "#", "#", "#"]
+        ]
+        self.mark_field = [
+            ["#", "#", "#", "#", "#", "#"],
+            ["#", "#", "#", "#", "#", "#"],
+            ["#", "#", "#", "#", "#", "#"],
+            ["#", "#", "#", "#", "#", "#"],
+            ["#", "#", "#", "#", "#", "#"],
+            ["#", "#", "#", "#", "#", "#"]
+        ]
+        self.ship_location = {
+        }
+    coordinates = ["a","b","c","d","e","f"]
+
+
+class Game():
+    def field(name):
+        if name == player1.player_name:
+            current_field = field1.field
+            current_move = player1.player_name
+        else:
+            current_field = field2.field
+            current_move = player2.player_name
+        return current_move, current_field
+
+    def beggin():
+        player1.player_name = str(input("Введите имя первого игрока: "));
+        player2.player_name = str(input("Введите имя второго игрока: "));
+        for i in range(1, Player.big_ship+1):
+            field1.ship_location.setdefault(f"bs{i}", [])
+            field2.ship_location.setdefault(f"bs{i}", [])
+        for i in range(1, Player.medium_ship+1):
+            field1.ship_location.setdefault(f"ms{i}", [])
+            field2.ship_location.setdefault(f"ms{i}", [])
+        for i in range(1, Player.small_ship + 1):
+            field1.ship_location.setdefault(f"ss{i}", [])
+            field2.ship_location.setdefault(f"ss{i}", [])
+
+    def load():
+        sys.stdout.write("Загрузка")
+        for i in range(3):
+            time.sleep(.05)
+            sys.stdout.write('.')
+            # sys.stdout.flush()
+            time.sleep(.25)
+        sys.stdout.write('\n')
+
+    def move(player_name):
+        print(f"Ходит: {player_name}!")
+
+    def get_cords():
+        cord = []
+        while cord != None:
+            try:
+                cord = [str(input("Введите координату (Буква слева): ")),
+                        int(input("Введите координату (Цифра сверху): "))]
+                if cord[0] not in Field.coordinates or (cord[1] < 0 or cord[1] > 6):
+                    print("Ошибка")
+                else:
+                    #print("Координаты записаны!")
+                    break
+            except ValueError:
+                print("Ошибка")
+        return cord
+
+    def create_move(player_move, field, cs, direct):
+        Game.display_field(field)
+        if cs == "1":
+            print(f"\033[1m\033[31m{'Корабль: Большой @@@'}\033[0m")
+            ship_size = 3
+        elif cs == "2":
+            print(f"\033[1m\033[31m{'Корабль: Средний @@'}\033[0m")
+            ship_size = 2
+        else:
+            print(f"\033[1m\033[31m{'Корабль: Малый @'}\033[0m")
+            ship_size = 1
+        if direct == "1":
+            print(f"\033[1m\033[31m{'Направление: Горизонтальный'}\033[0m")
+        else:
+            print(f"\033[1m\033[31m{'Направление: Вертикальный'}\033[0m")
+        cord = Game.get_cords()
+        if direct == "1":
+            x = Field.coordinates.index(cord[0])
+            y = cord[1] - 1
+            cm = Game.place_ship_horizonatal(y, x, ship_size, field)
+        else:
+            x = Field.coordinates.index(cord[0])
+            y = cord[1] - 1
+            cm = Game.place_ship_vertical(y, x, ship_size, field)
+        return cm
+
+
+
+    def place_ship_horizonatal(y, x, ship_size, field):
+        ship_coordinates = []
+        flag = False
+        break_flag = False
+        for i in range(x - 1, x + 2):
+            if break_flag:
+                break
+            if i < 0 or i >= 6:
+                continue
+            for j in range(y - 1, ship_size + y + 1):
+                if j < 0 or j >= 6:
+                    continue
+                if field[i][j] == "#":
+                    flag = True
+                else:
+                    flag = False
+                    break_flag = True
+                    print("Рядом стоимт корабль!")
+                    break
+        if flag:
+            if 6 - y >= ship_size:
+                for k in range(y, ship_size + y):
+                    field[x][k] = f"\033[1m\033[31m{'B'}\033[0m"
+                    ship_coordinates.append(x)
+                    ship_coordinates.append(k)
+                print("Корабль на воде!")
+            elif 6 - y < ship_size:
+                print("Не хватает места для корабля такого размера!")
+                flag = False
+        Game.display_field(field)
+        return flag, ship_coordinates
+
+    def place_ship_vertical(y, x, ship_size, field):
+        ship_coordinates = []
+        flag = False
+        break_flag = False
+        for i in range(x - 1, ship_size + x + 1):
+            if break_flag:
+                break
+            if i < 0 or i >= 6:
+                continue
+            for j in range(y - 1, y + 2):
+                if j < 0 or j >= 6:
+                    continue
+                if field[i][j] == "#":
+                    flag = True
+                else:
+                    flag = False
+                    break_flag = True
+                    print("Рядом стоимт корабль!")
+                    break
+        if flag:
+            if 6 - x >= ship_size:
+                for k in range(x, ship_size + x):
+                    field[k][y] = f"\033[1m\033[31m{'B'}\033[0m"
+                    ship_coordinates.append(k)
+                    ship_coordinates.append(y)
+            elif 6 - x < ship_size:
+                print("Не хватает места для корабля такого размера!")
+                flag = False
+        Game.display_field(field)
+        return flag, ship_coordinates
+
+    def display_field(field):
+        row = 0
+        colums = 0
+        for k in range(6):
+            row += 1
+            print('   ', f"{row}", end='')
+        print()
+
+        for i in field:
+            print(Field.coordinates[colums], end='   ')
+            colums += 1
+            for j in i:
+                print(j, end='    ')
+            print()
+    def set_value_cords(cords : list, player_name, ship_amount, ship_size):
+        if player_name == player1.player_name:
+            if ship_size == "1":
+                field1.ship_location[f"bs{ship_amount}"] += cords
+            elif ship_size == "2":
+                field1.ship_location[f"ms{ship_amount}"] += cords
+            elif ship_size == "3":
+                field1.ship_location[f"ss{ship_amount}"] += cords
+            #print(field1.ship_location)
+        else:
+            if ship_size == "1":
+                field2.ship_location[f"bs{ship_amount}"] += cords
+            elif ship_size == "2":
+                field2.ship_location[f"ms{ship_amount}"] += cords
+            elif ship_size == "3":
+                field2.ship_location[f"ss{ship_amount}"] += cords
+            #print(field2.ship_location)
+
+
+    def choose_ship(player_name, bs, ms, ss):
+        current_move = Game.field(player_name)[0]
+        current_field = Game.field(player_name)[1]
+        while bs != 0 or ms != 0 or ss != 0:
+            print("\n",f"Большие: {bs}. Средние: {ms}. Малые: {ss}.", "\n")
+            print("Для выбора введите цифру")
+            print(
+                f"{player_name}, выберите корабль : \n[1] - Больших кораблей: {bs} (@@@).\n[2] - Средних кораблей: {ms} (@@).\n[3] - Малых кораблей: {ss}. (@)", "\n")
+            choosen_ship = input()
+            if choosen_ship == "1" and bs != 0:
+                cd = Game.choose_direct(player_name, bs, ms, ss)
+                cm = Game.create_move(current_move,current_field, choosen_ship, cd)
+                Game.set_value_cords(cm[1], player_name, bs, choosen_ship)
+                if cm[0]:
+                    bs -= 1
+            elif choosen_ship == "2" and ms != 0:
+
+                cd = Game.choose_direct(player_name, bs, ms, ss)
+                cm = Game.create_move(Game.current_move, current_field, choosen_ship, cd)
+                Game.set_value_cords(cm[1], player_name, ms, choosen_ship)
+                if cm[0]:
+                    ms -= 1
+            elif choosen_ship == "3" and ss != 0:
+
+                cd = Game.choose_direct(player_name, bs, ms, ss)
+                cm = Game.create_move(Game.current_move, current_field, choosen_ship, "1")
+                Game.set_value_cords(cm[1], player_name, ss, choosen_ship)
+                if cm[0]:
+                    ss -= 1
+            else:
+                print("Такие корабли у вас кончились!!!")
+
+    def choose_direct(player_name, bs, ms, ss):
+        print("Для выбора введите цифру")
+        print(f"{player_name}, выберите направление: \n[1] - Горизонатльно\n[2] - Вертикально")
+        choosen_direct = (input())
+        if len(choosen_direct) != 1 and (choosen_direct != "1" or choosen_direct != "2"):
+            print("Ошибка!")
+            game1.choose_direct(player_name, bs, ms, ss)
+            #choose_direct(player_name, bs, ms, ss)
+        else:
+            return choosen_direct
+
+    def clearConsol():
+        if sys.platform == 'win32':
+            os.system('cls')
+        else:
+            os.system('clear')
+
+
+    def queue(count):
+        if count % 2 == 0:
+
+            current_field = field2.field
+            current_mark_field = field1.mark_field
+            current_move = player1.player_name
+        else:
+
+            current_field = field1\
+                .field
+            current_mark_field = field2.mark_field
+            current_move = player2.player_name
+
+        return current_move, current_mark_field, current_field
+
+
+    def shoot(mark_field, current_move, cords, field):
+        if field[cords[0]][cords[1]] == "#":
+            print("\n",'ПРОМАХ!',"\n")
+            mark_field[cords[0]][cords[1]] = "◯"
+            game1.display_field(mark_field)
+            return 0
+        else:
+            print("\n",'ПОПАДАНИЕ!',"\n")
+            mark_field[cords[0]][cords[1]] = "●"
+            if current_move == player1.player_name:
+                for key, value in field2.ship_location.items():
+                    for i in range(0, len(value) - 1, 2):
+                        if cords[0] == value[i] and cords[1] == value[i + 1]:
+                            value[i], value[i+1] = "-", "-"
+                            field2.ship_location[key] = value
+                for key, value in field2.ship_location.items():
+                    for i in value:
+                        if i == "-":
+                            flag = True
+                        else:
+                            flag = False
+                if flag:
+                    memory_key = key
+                    field2.ship_location.pop(key)
+                    player2.alive_ships -= 1
+                    print("\n", "УБИЛ!", "\n")
+                    return 3
+
+            else:
+                for key, value in field1.ship_location.items():
+                    for i in range(0, len(value) - 1, 2):
+                        if cords[0] == value[i] and cords[1] == value[i + 1]:
+                            value[i], value[i + 1] = "-", "-"
+                            field1.ship_location[key] = value
+                for key, value in field1.ship_location.items():
+                    for i in value:
+                        if i == "-":
+                            flag1 = True
+                        else:
+                            flag1 = False
+                if flag1:
+                    memory_key = key
+                    field1.ship_location.pop(key)
+                    player1.alive_ships -= 1
+                    print("\n","УБИЛ!","\n")
+                    return 3
+
+            game1.display_field(mark_field)
+            return 1
+
+player1 = Player()
+player2 = Player()
+field1 = Field()
+field2 = Field()
+
+game1 = Game
+class Main():
+    game1.beggin()
+    #game1.load()
+    game1.clearConsol()
+    game1.current_move = player1.player_name
+
+    print(f"\033[1m\033[31m{'Расстановка кораблей'}!\033[0m", "\n", f"Ходит игрок: {game1.current_move}", "\n")
+
+    game1.choose_ship(game1.current_move, player1.big_ship, player1.medium_ship, player1.small_ship)
+
+    print(f"Игрок: {game1.current_move} закончил расставлять корабли")
+    game1.clearConsol()
+
+    current_field = field2.field
+    game1.current_move = player2.player_name
+    game1.move(player2.player_name)
+    game1.choose_ship(game1.current_move, player2.big_ship, player2.medium_ship, player2.small_ship)
+
+    game1.clearConsol()
+    print(f"\033[1m\033[31m{'БОЙ НАЧИНАЕТСЯ'}!\033[0m")
+    game1.load()
+    game1.clearConsol()
+    current_field = field2.field
+    game1.current_move = player1.player_name
+    count = 0
+    game1.display_field(field1.mark_field)
+    flag = False
+    while player1.alive_ships != 0 and player2.alive_ships != 0:
+        q = game1.queue(count)
+        current_move = q[0]
+        current_mark_field = q[1]
+        current_field = q[2]
+        if flag == True:
+            game1.display_field(current_mark_field)
+
+        game1.move(current_move)
+
+        cords = game1.get_cords()
+        cords[0] = Field.coordinates.index(cords[0])
+        cords[1] = cords[1] -1
+        result = game1.shoot(current_mark_field, current_move, cords, current_field)
+        if result == 0:
+            flag = True
+            count += 1
+            print("Переход хода")
+        elif result == 1:
+            flag = False
+        else:
+            flag = True
+            print(f"Количество живых кораблей у игрока {player1.player_name}: {player1.alive_ships}", "\n")
+            print(f"Количество живых кораблей у игрока {player2.player_name}: {player2.alive_ships}", "\n")
+    if player1.alive_ships == 0:
+        print(f"\033[1m\033[31m{f'Победил Игрок: {player2.player_name}'}\033[0m")
+    else:
+        print(f"\033[1m\033[31m{f'Победил Игрок: {player2.player_name}'}\033[0m")
+    print(f"\033[1m\033[31m{'GAME OVER'}\033[0m")
+
+
+Main()
